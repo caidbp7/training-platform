ae47028492adf46f99922d0190fd4a88f7f028a6
caidbp7 <jmbrand@gmail.com>
2026-02-09 13:44:16 -0600

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import TrainingManagementSystem from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <TrainingManagementSystem />
+  </React.StrictMode>,
+)
