9275502f5c2c19b855827ff01cdacaeaaa40fe62
caidbp7 <jmbrand@gmail.com>
2026-02-11 10:43:32 -0600

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://mrqyikuumoslcmsqhucz.supabase.co'
-const supabaseKey = 'sb_publishable_aOvohhIMlkNN-7EsR0MVlg_cx4Bm5BE'
+export const supabaseUrl = 'https://mrqyikuumoslcmsqhucz.supabase.co'
+export const supabaseKey = 'sb_publishable_aOvohhIMlkNN-7EsR0MVlg_cx4Bm5BE'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
